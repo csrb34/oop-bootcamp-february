9116d06ac825b194d67f0ff302523ef54f23bf57
carla.s <carla.s@MacBook-Pro-de-carlas.local>
2022-02-21 10:44:06 +0100

    Add area method and test

--- src/main/java/oop/Rectagle.java
+++ src/main/java/oop/Rectagle.java
@@ -1,2 +1,17 @@
-package oop;public class Rectagle {
+package oop;
+
+public class Rectagle {
+    public Double height;
+    public Double width;
+    public String color;
+
+    public Rectagle(Double height, Double width, String color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    public Double getArea() {
+        return this.height*this.width;
+    }
 }
--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -1,2 +1,14 @@
-package oop;public class RectangleTest {
+package oop;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class RectangleTest {
+    @Test
+    public void itShouldCalculateTheArea() {
+        Rectagle rec1 = new Rectagle(2.0,3.0, "anyColor");
+
+        assertEquals(rec1.getArea(), 6.0);
+    }
 }
