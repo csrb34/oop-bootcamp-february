a593b3d05980df1f0097f3a28955c748352e3089
omrojasm <oscar.rojasmaillo@thoughtworks.com>
2022-02-21 11:56:01 +0100

    DiegoG/OscarR - Remove YAGNI

--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -7,13 +7,6 @@ import static org.testng.Assert.assertEquals;
 public class RectangleTest {
 
     @Test
-    public void shouldCalculateArea() {
-        var rectangle = new Rectangle(5, 10);
-
-        assertEquals(rectangle.calculateArea(), 50);
-    }
-
-    @Test
     public void shouldCalculatePerimeter() {
         var rectangle = new Rectangle(5, 10);
 
