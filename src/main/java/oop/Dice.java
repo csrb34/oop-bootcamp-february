092f3bd2f24815200a4d04b35d6041a436376dca
Esther Carrillo <esther.carrillo@thoughtworks.com>
2022-02-21 16:50:04 +0100

    [AJ/EC] Refactored dice class and its tests

--- src/main/java/oop/Dice.java
+++ src/main/java/oop/Dice.java
@@ -8,11 +8,6 @@ public class Dice {
     }
 
     public double calculateChance() {
-        if (numberOfSides == 1) return 100;
-        if (numberOfSides == 2) return 50;
-        if (numberOfSides == 3) return 33.33;
-        if (numberOfSides == 4) return 25;
-        if (numberOfSides == 5) return 20;
-        else return 16.67;
+        return ((double) 1/numberOfSides) * 100;
     }
 }
--- src/test/java/oop/DiceClass.java
+++ src/test/java/oop/DiceClass.java
@@ -7,38 +7,8 @@ import static org.testng.Assert.assertEquals;
 public class DiceClass {
 
     @Test
-    public void itShouldCalculateTheChanceFor1Side() {
-        Dice dice = new Dice(1);
-        assertEquals(dice.calculateChance(), 100);
-    }
-
-    @Test
-    public void itShouldCalculateTheChanceFor2Sides() {
-        Dice dice = new Dice(2);
-        assertEquals(dice.calculateChance(), 50);
-    }
-
-    @Test
-    public void itShouldCalculateTheChanceFor3Sides() {
-        Dice dice = new Dice(3);
-        assertEquals(dice.calculateChance(), 33.33);
-    }
-
-    @Test
-    public void itShouldCalculateTheChanceFor4Sides() {
-        Dice dice = new Dice(4);
-        assertEquals(dice.calculateChance(), 25);
-    }
-
-    @Test
-    public void itShouldCalculateTheChanceFor5Sides() {
-        Dice dice = new Dice(5);
-        assertEquals(dice.calculateChance(), 20);
-    }
-
-    @Test
     public void itShouldCalculateTheChanceFor6Sides() {
         Dice dice = new Dice(6);
-        assertEquals(dice.calculateChance(), 16.67);
+        assertEquals(dice.calculateChance(), 16.67, 2);
     }
 }
