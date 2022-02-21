f5ef375202873f80d61f68875bbb522a36d44f4a
DiegoG <diego.guida@thoughtworks.com>
2022-02-21 15:44:12 +0100

    OscarR/DiegoG - Testing improvements

--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -1,5 +1,6 @@
 package oop;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -7,28 +8,35 @@ import static org.testng.Assert.assertThrows;
 
 public class RectangleTest {
 
+    public static final int WIDTH = 5;
+    public static final int HEIGHT = 10;
+    public static final int AREA = 50;
+    public static final int PERIMETER = 30;
+    private Rectangle rectangle;
+
+    @BeforeTest
+    public void setUp() {
+        rectangle = new Rectangle(WIDTH, HEIGHT);
+    }
+
     @Test
     public void shouldCalculateArea() {
-        var rectangle = new Rectangle(5, 10);
-
-        assertEquals(rectangle.calculateArea(), 50);
+        assertEquals(rectangle.calculateArea(), AREA);
     }
 
     @Test
     public void shouldCalculatePerimeter() {
-        var rectangle = new Rectangle(5, 10);
-
-        assertEquals(rectangle.calculatePerimeter(), 30);
+        assertEquals(rectangle.calculatePerimeter(), PERIMETER);
     }
 
     @Test
     public void itShouldNotAcceptNegativeWidth() {
-        assertThrows(IllegalArgumentException.class, () -> new Rectangle(-5, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(-WIDTH, HEIGHT));
 
     }
 
     @Test
     public void itShouldNotAcceptNegativeHeight() {
-        assertThrows(IllegalArgumentException.class, () -> new Rectangle(5, -10));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(WIDTH, -HEIGHT));
     }
 }
