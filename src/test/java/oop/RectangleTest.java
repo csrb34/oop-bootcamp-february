63b15ffbcebe6acb1c7de23dcc0f27ae22bf7a96
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-21 12:44:49 +0100

    Revert "Removes calculateArea"

--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -7,6 +7,13 @@ import static org.testng.Assert.assertEquals;
 public class RectangleTest {
 
     @Test
+    public void itShouldCalculateTheArea() {
+        Rectangle rectangle = new Rectangle(2.0, 3.0);
+
+        assertEquals(rectangle.calculateArea(), 6.0);
+    }
+
+    @Test
     public void itShouldCalculateThePerimeter() {
         Rectangle rectangle = new Rectangle(2.0, 3.0);
 
