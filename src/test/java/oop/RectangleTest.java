582710aa1a529829a9a38c2e73d04ce69207dfba
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-21 11:55:29 +0100

    tests creation of a rectangle

--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -1,10 +1,18 @@
 package oop;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class RectangleTest {
+    @Test
+    public void itShouldCreateARectangle() {
+        Rectangle rec1 = new Rectangle(2.0, 3.0, "anyColor");
+
+        Assert.assertTrue(rec1 instanceof Rectangle);
+
+    }
     @Test
     public void itShouldCalculateTheArea() {
         Rectangle rec1 = new Rectangle(2.0, 3.0, "anyColor");
