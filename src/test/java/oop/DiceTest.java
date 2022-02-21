61476b006fb7591ae5adfb414ff5b72a73195210
Esther Carrillo <esther.carrillo@thoughtworks.com>
2022-02-21 16:58:55 +0100

    [AJ/EC] Added test of the probability of not getting a 6

--- src/test/java/oop/DiceTest.java
+++ src/test/java/oop/DiceTest.java
@@ -4,11 +4,18 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class DiceClass {
+public class DiceTest {
 
     @Test
     public void itShouldCalculateTheChanceFor6Sides() {
         Dice dice = new Dice(6);
         assertEquals(dice.calculateChance(), 16.67, 2);
+    }
+
+    @Test
+    public void itShouldCalculateTheChanceOfNotGettingA6() {
+        Dice dice = new Dice(6);
+        assertEquals(100 - dice.calculateChance(), 83.33, 2);
+
     }
 }
