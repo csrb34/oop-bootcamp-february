af9bb88e2e94f7e471ea5cbf5cc11d2eab0ab3ac
Jredfern-gl <jenny.redfern@providers.glovoapp.com>
2022-02-22 12:40:19 +0100

    (Carla/Jen)-conversion-solution

--- src/main/java/oop/Conversion.java
+++ src/main/java/oop/Conversion.java
@@ -1,2 +1,15 @@
-package oop;public class Conversion {
+package oop;
+
+public class Conversion {
+
+    private static final double CONVERSION_FROM_FEET_TO_METRES = 0.3048;
+    private final double feet;
+
+    public Conversion(double feet) {
+        this.feet = feet;
+    }
+
+    public double convertFeetToMetres() {
+        return feet * CONVERSION_FROM_FEET_TO_METRES;
+    }
 }
--- src/test/java/oop/ConversionTest.java
+++ src/test/java/oop/ConversionTest.java
@@ -1,4 +1,17 @@
+package oop;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class ConversionTest {
-    
+
+    @Test
+    public void itShouldConvertFeetToMetres() {
+        double expectedMetres = 0.3048;
+        double feet = 1;
+
+        Conversion conversion = new Conversion(feet);
+        assertEquals(conversion.convertFeetToMetres(), expectedMetres);
+    }
 }
