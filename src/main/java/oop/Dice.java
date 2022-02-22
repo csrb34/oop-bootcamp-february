77e2aca71227ed313cac3ff5ab08dd67d4f8edce
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-22 09:17:32 +0100

    Refactor methods

--- src/main/java/oop/Dice.java
+++ src/main/java/oop/Dice.java
@@ -2,12 +2,17 @@ package oop;
 
 public class Dice {
     private final int numberOfSides;
+    int SURE_EVENT_PROBABILITY = 100;
 
     public Dice(int numberOfSides) {
         this.numberOfSides = numberOfSides;
     }
 
-    public double calculateChance() {
-        return ((double) 1/numberOfSides) * 100;
+    public double calculateChanceOfHavingANumber() {
+        return ((double) 1 / numberOfSides) * SURE_EVENT_PROBABILITY;
+    }
+
+    public double calculateChanceOfNotHavingANumber() {
+        return SURE_EVENT_PROBABILITY - calculateChanceOfHavingANumber();
     }
 }
--- src/test/java/oop/DiceTest.java
+++ src/test/java/oop/DiceTest.java
@@ -9,13 +9,13 @@ public class DiceTest {
     @Test
     public void itShouldCalculateTheChanceFor6Sides() {
         Dice dice = new Dice(6);
-        assertEquals(dice.calculateChance(), 16.67, 2);
+        assertEquals(dice.calculateChanceOfHavingANumber(), 16.67, 2);
     }
 
     @Test
     public void itShouldCalculateTheChanceOfNotGettingA6() {
         Dice dice = new Dice(6);
-        assertEquals(100 - dice.calculateChance(), 83.33, 2);
+        assertEquals(dice.calculateChanceOfNotHavingANumber(), 83.33, 2);
 
     }
 }
