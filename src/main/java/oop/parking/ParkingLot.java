70706effefd5b07e36792667eb942f7729307ddc
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-23 17:49:13 +0100

    Revert "Adds notification sender in the parking lot constructor"
    This reverts commit 0a8d876d242b33730e425610142615327cf066c0.

--- src/main/java/oop/parking/ParkingLot.java
+++ src/main/java/oop/parking/ParkingLot.java
@@ -4,13 +4,11 @@ public class ParkingLot {
     private final int id;
     private final int totalCapacity;
     private int freeSpots;
-    private final NotificationSender notificationSender;
 
-    public ParkingLot(int id, int totalCapacity, int freeSpots, NotificationSender notificationSender) {
+    public ParkingLot(int id, int totalCapacity, int freeSpots) {
         this.id = id;
         this.totalCapacity = totalCapacity;
         this.freeSpots = freeSpots;
-        this.notificationSender = notificationSender;
     }
 
     public double getAvailabilityPercentage() {
@@ -19,7 +17,6 @@ public class ParkingLot {
 
     public void fillSpot() {
         freeSpots -= 1;
-        if (getAvailabilityPercentage() <= 0.25) notificationSender.notifyOverUsed(id);
     }
 
     public int getId() {
--- src/test/java/oop/parking/ParkingLotTest.java
+++ src/test/java/oop/parking/ParkingLotTest.java
@@ -1,10 +1,7 @@
 package oop.parking;
 
-import org.mockito.Mockito;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertEquals;
 
@@ -12,35 +9,30 @@ public class ParkingLotTest {
 
     public static final double DELTA = 0.01;
 
-    private NotificationSender notificationSender;
-
-    @BeforeMethod
-    public void setUp() {
-        notificationSender = mock(NotificationSender.class);
-    }
+    private final NotificationSender notificationSender = new NotificationSender();
 
     @Test
     public void itShouldReturnOccupationPercentage() {
-        ParkingLot parkingLot = new ParkingLot(1, 10, 10, notificationSender);
+        ParkingLot parkingLot = new ParkingLot(1,10,10);
         assertEquals(parkingLot.getAvailabilityPercentage(), 1d, DELTA);
     }
 
     @Test
     public void itShouldReturnOccupationPercentageForHalfOccupation() {
-        ParkingLot parkingLot = new ParkingLot(1, 10, 5, notificationSender);
+        ParkingLot parkingLot = new ParkingLot(1,10,5);
         assertEquals(parkingLot.getAvailabilityPercentage(), 0.5d, DELTA);
     }
 
     @Test
     public void itShouldDecreaseTheAvailableSpotsInOneUnit() {
-        ParkingLot parkingLot = new ParkingLot(1, 10, 5, notificationSender);
+        ParkingLot parkingLot = new ParkingLot(1,10,5);
         parkingLot.fillSpot();
         assertEquals(parkingLot.getAvailabilityPercentage(), 0.4d, DELTA);
     }
 
     @Test
     public void itShouldShouldNotifyWhenOccupationIsOver75Percent() {
-        ParkingLot parkingLot = new ParkingLot(1, 10, 3, notificationSender);
+        ParkingLot parkingLot = new ParkingLot(1,5,3);
         parkingLot.fillSpot();
         verify(notificationSender).notifyOverUsed(parkingLot.getId());
     }
