0a8d876d242b33730e425610142615327cf066c0
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-23 17:42:15 +0100

    Adds notification sender in the parking lot constructor

--- src/test/java/oop/parking/ParkingAssistantTest.java
+++ src/test/java/oop/parking/ParkingAssistantTest.java
@@ -6,23 +6,24 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
 
 public class ParkingAssistantTest {
 
     private  Car myCar;
+    private  NotificationSender notificationSender;
 
     @BeforeMethod
     public void setUp() {
         myCar = Mockito.mock(Car.class);
+        notificationSender = mock(NotificationSender.class);
     }
 
     @Test
     public void itShouldParkACarInAParkingLotWhenOcupationIsLessThan80Percent() {
 
-        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,10, 5));
+        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,10, 5, notificationSender));
         var assistant = new ParkingAssistant(parkingLots);
 
         assistant.park(myCar);
@@ -32,7 +33,7 @@ public class ParkingAssistantTest {
 
     @Test
     public void itShouldNotParkACarParkingIsMoreThan80PercentFull() {
-        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,10, 1));
+        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,10, 1, notificationSender));
 
         var assistant = new ParkingAssistant(parkingLots);
 
@@ -44,7 +45,7 @@ public class ParkingAssistantTest {
     @Test
     public void itShouldParkACarInTheSecondAParkingLot() {
 
-        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,6, 1), new ParkingLot(2,10,10));
+        final List<ParkingLot> parkingLots = List.of(new ParkingLot(1,6, 1, notificationSender), new ParkingLot(2,10,10, notificationSender));
         var assistant = new ParkingAssistant(parkingLots);
 
         final var parkingLotId = assistant.park(myCar);
--- src/test/java/oop/parking/ParkingLotTest.java
+++ src/test/java/oop/parking/ParkingLotTest.java
@@ -1,7 +1,10 @@
 package oop.parking;
 
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertEquals;
 
@@ -9,30 +12,35 @@ public class ParkingLotTest {
 
     public static final double DELTA = 0.01;
 
-    private final NotificationSender notificationSender = new NotificationSender();
+    private NotificationSender notificationSender;
+
+    @BeforeMethod
+    public void setUp() {
+        notificationSender = mock(NotificationSender.class);
+    }
 
     @Test
     public void itShouldReturnOccupationPercentage() {
-        ParkingLot parkingLot = new ParkingLot(1,10,10);
+        ParkingLot parkingLot = new ParkingLot(1, 10, 10, notificationSender);
         assertEquals(parkingLot.getAvailabilityPercentage(), 1d, DELTA);
     }
 
     @Test
     public void itShouldReturnOccupationPercentageForHalfOccupation() {
-        ParkingLot parkingLot = new ParkingLot(1,10,5);
+        ParkingLot parkingLot = new ParkingLot(1, 10, 5, notificationSender);
         assertEquals(parkingLot.getAvailabilityPercentage(), 0.5d, DELTA);
     }
 
     @Test
     public void itShouldDecreaseTheAvailableSpotsInOneUnit() {
-        ParkingLot parkingLot = new ParkingLot(1,10,5);
+        ParkingLot parkingLot = new ParkingLot(1, 10, 5, notificationSender);
         parkingLot.fillSpot();
         assertEquals(parkingLot.getAvailabilityPercentage(), 0.4d, DELTA);
     }
 
     @Test
     public void itShouldShouldNotifyWhenOccupationIsOver75Percent() {
-        ParkingLot parkingLot = new ParkingLot(1,5,3);
+        ParkingLot parkingLot = new ParkingLot(1, 10, 3, notificationSender);
         parkingLot.fillSpot();
         verify(notificationSender).notifyOverUsed(parkingLot.getId());
     }
