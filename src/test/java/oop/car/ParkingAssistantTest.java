9bc49a66739f05f8cfca2db92e3d4c04c278670e
Carla San Roman <carla.s@thoughtworks.com>
2022-02-23 15:36:00 +0100

    CarlaS/DiegoG/Ben - Add mocks for ParkingAssistantTest

--- src/test/java/oop/car/ParkingAssistantTest.java
+++ src/test/java/oop/car/ParkingAssistantTest.java
@@ -3,7 +3,9 @@ package oop.car;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -21,12 +23,14 @@ public class ParkingAssistantTest {
 
     @Test
     public void itShouldBeAbleToParkACar() {
+        when(parkingLot.parkCar(anyInt())).thenReturn(true);
         parkingAssistant.addParkingLot(parkingLot);
         assertTrue(parkingAssistant.parkCar(5));
     }
 
     @Test
     public void itShouldNotParkACarWhenNoSpaceLeft() {
+        when(parkingLot.parkCar(anyInt())).thenReturn(true).thenReturn(false);
         parkingAssistant.addParkingLot(parkingLot);
         assertTrue(parkingAssistant.parkCar(5));
         assertFalse(parkingAssistant.parkCar(5));
@@ -34,9 +38,11 @@ public class ParkingAssistantTest {
 
     @Test
     public void itShouldParkACarOnAvailableParkingSlot() {
+        when(parkingLot.parkCar(anyInt())).thenReturn(false);
+        ParkingLot parkingLot2 = mock(ParkingLot.class);
+        when(parkingLot2.parkCar(anyInt())).thenReturn(true);
         parkingAssistant.addParkingLot(parkingLot);
-        parkingAssistant.addParkingLot(new ParkingLot(2));
-        assertTrue(parkingAssistant.parkCar(5));
+        parkingAssistant.addParkingLot(parkingLot2);
         assertTrue(parkingAssistant.parkCar(5));
     }
 }
