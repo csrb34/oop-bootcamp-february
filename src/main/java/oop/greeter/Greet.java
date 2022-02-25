67b866cfe73552a226bcc133732603d08e9c5ecd
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2022-02-25 12:11:54 +0100

    Refactoring

--- src/main/java/oop/greeter/Greet.java
+++ src/main/java/oop/greeter/Greet.java
@@ -12,21 +12,21 @@ public class Greet {
     }
 
     public String greet(String name) {
-        int hour = LocalDateTime.now(clock).getHour();
-        String nameUpdated = formatName(name);
+        int currentHour = LocalDateTime.now(clock).getHour();
+        String nameFormatted = formatName(name);
 
-        if (6 <= hour && hour < 12) {
-            return String.format("Good morning %s", nameUpdated);
+        if (6 <= currentHour && currentHour < 12) {
+            return String.format("Good morning %s", nameFormatted);
         }
-        if (18 <= hour && hour < 22) {
-            return String.format("Good evening %s", nameUpdated);
-        }
-
-        if (22 <= hour && hour < 24 || 0 <= hour && hour < 6) {
-            return String.format("Good night %s", nameUpdated);
+        if (18 <= currentHour && currentHour < 22) {
+            return String.format("Good evening %s", nameFormatted);
         }
 
-        return String.format("Hello %s", nameUpdated);
+        if (22 <= currentHour && currentHour < 24 || 0 <= currentHour && currentHour < 6) {
+            return String.format("Good night %s", nameFormatted);
+        }
+
+        return String.format("Hello %s", nameFormatted);
     }
 
 
@@ -37,4 +37,7 @@ public class Greet {
     private String capitaliseFirstLetter(String name) {
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
+
+//    private static final Pair<LocalTime, LocalTime> MORNING_LIMITS = new ImmutablePair(LocalTime.of(6, 0), LocalTime.of(12, 0));
+//    private static final Pair<LocalTime, LocalTime> EVENING_LIMITS = new ImmutablePair(LocalTime.of(12, 0), LocalTime.of(22, 0));
 }
