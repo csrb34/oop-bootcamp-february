7555630e8a90c3d2d2d73ec090a76a94a6248c01
Carla San Roman <carla.s@thoughtworks.com>
2022-02-25 15:45:43 +0100

    AnaJ/IreneT/CarlaS - Add a map to trasnalate the characters to Nato Alphabet

--- src/main/java/oop/Nato/NatoAlphabet.java
+++ src/main/java/oop/Nato/NatoAlphabet.java
@@ -1,13 +1,60 @@
 package oop.Nato;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class NatoAlphabet {
+    private static final Map<String, String> NATO_ALPHABET = ImmutableMap.ofEntries(
+            entry("a", "alpha"),
+            entry("b", "bravo"),
+            entry("c", "charlie"),
+            entry("d", "delta"),
+            entry("e", "echo"),
+            entry("f", "foxtrot"),
+            entry("g", "golf"),
+            entry("h", "hotel"),
+            entry("i", "india"),
+            entry("j", "juliett"),
+            entry("k", "kilo"),
+            entry("l", "lima"),
+            entry("m", "mike"),
+            entry("n", "november"),
+            entry("o", "oscar"),
+            entry("p", "papa"),
+            entry("q", "quebec"),
+            entry("r", "romeo"),
+            entry("s", "sierra"),
+            entry("t", "tango"),
+            entry("u", "uniform"),
+            entry("v", "victor"),
+            entry("w", "whiskey"),
+            entry("x", "xray"),
+            entry("y", "yankee"),
+            entry("z", "zulu"),
+            entry("1", "one"),
+            entry("2", "two"),
+            entry("3", "three"),
+            entry("4", "four"),
+            entry("5", "five"),
+            entry("6", "six"),
+            entry("7", "seven"),
+            entry("8", "eight"),
+            entry("9", "nine"),
+            entry("0", "zero"),
+            entry(" ", "null")
+    );
 
     public static String encode(String word) {
         if (word.isBlank()) throw new IllegalArgumentException("input outside NATO alphabet");
 
-        if (word.equals("hello")) return "hotel-echo-lima-lima-oscar";
-
-        return "kilo-hotel-alpha-bravo-india-bravo-null-november-uniform-romeo-mike-alpha-golf-oscar-mike-echo-delta-oscar-victor";
+        String natoWord = "";
+        for (int i =0; i < word.length(); i++){
+            natoWord += NATO_ALPHABET.get(String.format("%c", word.charAt(i))) + "-";
+        }
+        return natoWord.substring(0,natoWord.length()-1);
     }
 
     public static String decode(String natoWord) {
